2dbc27b32a6f9cba118491c4572267cea39c4d0e
Yohan910 <yohan.dacosta910@gmail.com>
2024-07-18 22:42:58 +0200

    Created loading and toast component and created custom hook useAxios

--- market/src/hooks/useAxios.js
+++ market/src/hooks/useAxios.js
@@ -1,15 +1,25 @@
 import { useState, useEffect } from "react";
 import api from "../services/api";
+import axios from "axios";
 
 const useAxios = () => {
     const [ data, setData ] = useState(null);
-    const [ error, setError ] = useState("");
+    const [ errors, setErrors ] = useState("");
     const [ loading, setLoading ] = useState(false);
-
+    
     const { axios_instance } = api();
+
+    let controller = new AbortController();
+
+    useEffect(() => {
+        return () => controller?.abort();
+    }, []);
 
     const fetchData = async({url, method, data = {}, params = {}}) => {
         setLoading(true);
+
+        controller.abort();
+        controller = new AbortController();
 
         try {
             const response = await axios_instance({
@@ -17,16 +27,21 @@ const useAxios = () => {
                 method,
                 data,
                 params,
+                signal: controller.signal,
             });
             setData(response.data);   
         } catch (error) {
-            setError(error.response ? error.response.data : error.message);
+            if (axios.isCancel(error)) {
+                console.log("Request cancelled", error.message);
+            }else {
+                setErrors(error.response ? error.response.data : error.message);
+            }
         } finally {
             setLoading(false);
         }
     }
 
-    return {data, error, loading, fetchData};
+    return {data, errors, loading, fetchData};
 };
 
 export default useAxios;
--- market/src/pages/Product.jsx
+++ market/src/pages/Product.jsx
@@ -1,14 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { Suspense, useEffect } from 'react';
 import ButtonAction from '../components/common/ButtonAction';
 import useAxios from '../hooks/useAxios';
+import Loading from '../components/common/Loading';
+import Toast from '../components/common/Toast';
 
 const Product = () => {
-    const { data, error, loading, fetchData } = useAxios();
+    const { data, errors, fetchData } = useAxios();
 
     useEffect(() => {
         fetchData({
             url: "/products",
-            method: "GET",
+            method: "GET"
         });
     }, []);
 
@@ -20,6 +22,7 @@ const Product = () => {
 
     return (
         <>
+            <Toast errors={errors} />
             <div>
                 <div className='flex mt-4 mb-4'>
                     <div className="flex-1 pr-4">
@@ -38,74 +41,79 @@ const Product = () => {
                     <div className='text-2xl mr-4 text-center font-bold'>List Products</div>
 
                 </div>
-                <table className="border-collapse table-auto w-full whitespace-no-wrap bg-white table-striped relative">
-                    <thead>
-                        <tr className="text-left">
-                            <th className="py-2 px-3 sticky top-0 border-b border-gray-200 bg-gray-100">
-                                <label
-                                    className="text-teal-500 inline-flex justify-between items-center hover:bg-gray-200 px-2 py-2 rounded-lg cursor-pointer">
-                                    <input type="checkbox" name="" id="" className="form-checkbox" />
-                                </label>
-                            </th>
-                            <th className="bg-gray-100 sticky top-0 border-b border-gray-200 px-6 py-2 text-gray-600 font-bold tracking-wider uppercase text-xs">
-                                <span className=' col-span-10'>USER ID</span>
-                            </th>
-                            <th className="bg-gray-100 sticky top-0 border-b border-gray-200 px-6 py-2 text-gray-600 font-bold tracking-wider uppercase text-xs">
-                                <span className=' col-span-10'>FIRSTNAME</span>
-                            </th>
-                            <th className="bg-gray-100 sticky top-0 border-b border-gray-200 px-6 py-2 text-gray-600 font-bold tracking-wider uppercase text-xs">
-                                <span className=' col-span-10'>LASTNAME</span>
-                            </th>
-                            <th className="bg-gray-100 sticky top-0 border-b border-gray-200 px-6 py-2 text-gray-600 font-bold tracking-wider uppercase text-xs">
-                                <span className=' col-span-10'>EMAIL</span>
-                            </th>
-                            <th className="bg-gray-100 sticky top-0 border-b border-gray-200 px-6 py-2 text-gray-600 font-bold tracking-wider uppercase text-xs">
-                                <span className=' col-span-10'>GENDER</span>
-                            </th>
-                            <th className="bg-gray-100 sticky top-0 border-b border-gray-200 px-6 py-2 text-gray-600 font-bold tracking-wider uppercase text-xs">
-                                <span className=' col-span-10'>PHONE</span>
-                            </th>
-                            <th className="bg-gray-100 sticky top-0 border-b border-gray-200 px-6 py-2 text-gray-600 font-bold tracking-wider uppercase text-xs">
-                                <span className=' col-span-10'>Actions</span>
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td className="border-dashed border-t border-gray-200 px-3">
-                                <label
-                                    className="text-teal-500 inline-flex justify-between items-center hover:bg-gray-200 px-2 py-2 rounded-lg cursor-pointer">
-                                    <input type="checkbox" className="form-checkbox" name="" id="" />
-                                </label>
-                            </td>
-                            <td className="border-dashed border-t border-gray-200 userId">
-                                <span className="text-gray-700 px-6 py-3 flex items-center">1</span>
-                            </td>
-                            <td className="border-dashed border-t border-gray-200 firstName">
-                                <span className="text-gray-700 px-6 py-3 flex items-center">Brianne</span>
-                            </td>
-                            <td className="border-dashed border-t border-gray-200 lastName">
-                                <span className="text-gray-700 px-6 py-3 flex items-center">Dzeniskevich</span>
-                            </td>
-                            <td className="border-dashed border-t border-gray-200 emailAddress">
-                                <span className="text-gray-700 px-6 py-3 flex items-center">bdzeniskevich1@hostgator.com</span>
-                            </td>
-                            <td className="border-dashed border-t border-gray-200 gender">
-                                <span className="text-gray-700 px-6 py-3 flex items-center">Female</span>
-                            </td>
-                            <td className="border-dashed border-t border-gray-200 phoneNumber">
-                                <span className="text-gray-700 px-6 py-3 flex items-center">144-190-8956</span>
-                            </td>
-                            <td className="border-dashed border-t border-gray-200 phoneNumber">
-                                <div className='flex'>
-                                    <ButtonAction label="Add" onClick={onHandleClick} />
-                                    <ButtonAction label="Edit" color="bg-blue-500" onClick={onHandleClick} />
-                                    <ButtonAction label="Delete" color="bg-red-500" onClick={onHandleClick} />
-                                </div>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
+                <Suspense fallback={<Loading />}>
+                    <table className="border-collapse table-auto w-full whitespace-no-wrap bg-white table-striped relative">
+                        <thead>
+                            <tr className="text-left">
+                                <th className="py-2 px-3 sticky top-0 border-b border-gray-200 bg-gray-100">
+                                    <label
+                                        className="text-teal-500 inline-flex justify-between items-center hover:bg-gray-200 px-2 py-2 rounded-lg cursor-pointer">
+                                        <input type="checkbox" name="" id="" className="form-checkbox" />
+                                    </label>
+                                </th>
+                                <th className="bg-gray-100 sticky top-0 border-b border-gray-200 px-6 py-2 text-gray-600 font-bold tracking-wider uppercase text-xs">
+                                    <span className=' col-span-10'>ID</span>
+                                </th>
+                                <th className="bg-gray-100 sticky top-0 border-b border-gray-200 px-6 py-2 text-gray-600 font-bold tracking-wider uppercase text-xs">
+                                    <span className=' col-span-10'>NAME</span>
+                                </th>
+                                <th className="bg-gray-100 sticky top-0 border-b border-gray-200 px-6 py-2 text-gray-600 font-bold tracking-wider uppercase text-xs">
+                                    <span className=' col-span-10'>COMPANY</span>
+                                </th>
+                                <th className="bg-gray-100 sticky top-0 border-b border-gray-200 px-6 py-2 text-gray-600 font-bold tracking-wider uppercase text-xs">
+                                    <span className=' col-span-10'>TYPE PRODUCT</span>
+                                </th>
+                                <th className="bg-gray-100 sticky top-0 border-b border-gray-200 px-6 py-2 text-gray-600 font-bold tracking-wider uppercase text-xs">
+                                    <span className=' col-span-10'>COUNTRY</span>
+                                </th>
+                                <th className="bg-gray-100 sticky top-0 border-b border-gray-200 px-6 py-2 text-gray-600 font-bold tracking-wider uppercase text-xs">
+                                    <span className=' col-span-10'>QUANTITY</span>
+                                </th>
+                                <th className="bg-gray-100 sticky top-0 border-b border-gray-200 px-6 py-2 text-gray-600 font-bold tracking-wider uppercase text-xs">
+                                    <span className=' col-span-10'>Actions</span>
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {!data?.errors && data?.data?.map((product, idx) =>
+                            (<tr key={product.id + idx}>
+                                <td key={product.id + product.name} className="border-dashed border-t border-gray-200 px-3">
+                                    <label
+                                        className="text-teal-500 inline-flex justify-between items-center hover:bg-gray-200 px-2 py-2 rounded-lg cursor-pointer">
+                                        <input type="checkbox" className="form-checkbox" name="" id="" />
+                                    </label>
+                                </td>
+                                <td key={product.id + "productId"} className="border-dashed border-t border-gray-200 productId">
+                                    <span className="text-gray-700 px-6 py-3 flex items-center">{product.id}</span>
+                                </td>
+                                <td key={product.id + "name"} className="border-dashed border-t border-gray-200 name">
+                                    <span className="text-gray-700 px-6 py-3 flex items-center">{product.name}</span>
+                                </td>
+                                <td key={product.id + "companyId"} className="border-dashed border-t border-gray-200 companyId">
+                                    <span className="text-gray-700 px-6 py-3 flex items-center">{product.company_id}</span>
+                                </td>
+                                <td key={product.id + "typeProduct"} className="border-dashed border-t border-gray-200 typeProduct">
+                                    <span className="text-gray-700 px-6 py-3 flex items-center">{product?.type_product}</span>
+                                </td>
+                                <td key={product.id + "countryId"} className="border-dashed border-t border-gray-200 countryId">
+                                    <span className="text-gray-700 px-6 py-3 flex items-center">{product.country_id}</span>
+                                </td>
+                                <td key={product.id + "quantity"} className="border-dashed border-t border-gray-200 quantity">
+                                    <span className="text-gray-700 px-6 py-3 flex items-center">{product.quantity}</span>
+                                </td>
+                                <td key={product.id + "actions"} className="border-dashed border-t border-gray-200 actions">
+                                    <div className='flex'>
+                                        <ButtonAction label="Add" onClick={onHandleClick} />
+                                        <ButtonAction label="Edit" color="bg-blue-500" onClick={onHandleClick} />
+                                        <ButtonAction label="Delete" color="bg-red-500" onClick={onHandleClick} />
+                                    </div>
+                                </td>
+                            </tr>))
+                            }
+                        </tbody>
+                    </table>
+                </Suspense>
+
             </div>
         </>
     )
