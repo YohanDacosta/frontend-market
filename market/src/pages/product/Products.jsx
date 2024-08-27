ca417c5e580059e44cc7fec09bb73b6698881e7e
Yohan910 <yohan.dacosta910@gmail.com>
2024-08-27 16:06:23 +0200

    Moving the Suspense and Loading of the position

--- market/src/pages/product/Products.jsx
+++ market/src/pages/product/Products.jsx
@@ -74,26 +74,28 @@ const Products = () => {
 
     return (
         <>
-            <div>
-                <div className='flex mt-4 mb-4'>
-                    <div className="flex-1 pr-4">
-                        <div className="relative md:w-1/3">
-                            <input type="search" placeholder="Search..." className='w-full pl-10 pr-4 py-2 rounded-lg shadow focus:outline-non text-gray-600 font-medium' />
-                            <div className="absolute top-0 left-0 inline-flex items-center p-2">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 text-gray-400" viewBox="0 0 24 24"
-                                    fill="none">
-                                    <rect x="0" y="0" width="24" height="24"></rect>
-                                    <circle cx="10" cy="10" r="7" />
-                                    <line x1="21" y1="21" x2="15" y2="15" />
-                                </svg>
+            <Suspense fallback={Loading}>
+                <div>
+                    <div className='flex mt-4 mb-4'>
+                        <div className="flex-1 pr-4">
+                            <div className="relative md:w-1/3">
+                                <input type="search" placeholder="Search..." className='w-full pl-10 pr-4 py-2 rounded-lg shadow focus:outline-non text-gray-600 font-medium' />
+                                <div className="absolute top-0 left-0 inline-flex items-center p-2">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 text-gray-400" viewBox="0 0 24 24"
+                                        fill="none">
+                                        <rect x="0" y="0" width="24" height="24"></rect>
+                                        <circle cx="10" cy="10" r="7" />
+                                        <line x1="21" y1="21" x2="15" y2="15" />
+                                    </svg>
+                                </div>
                             </div>
                         </div>
+                        <div className='text-2xl mr-4 text-center font-bold'>List Products</div>
+                        <ButtonAction label="New Product" onClick={handleClickAdd} />
                     </div>
-                    <div className='text-2xl mr-4 text-center font-bold'>List Products</div>
-                    <ButtonAction label="New Product" onClick={handleClickAdd} />
-                </div>
-                <CustomModal show={show} setShow={setShow} fnc={onHandleDelete} />
-                <Suspense fallback={Loading}>
+
+                    <CustomModal show={show} setShow={setShow} fnc={onHandleDelete} />
+
                     <table className="border-collapse table-auto w-full whitespace-no-wrap bg-white table-striped relative">
                         <thead>
                             <tr className="text-left">
@@ -164,10 +166,9 @@ const Products = () => {
                             }
                         </tbody>
                     </table>
-                </Suspense>
-
-            </div>
-            <Outlet />
+                </div>
+                <Outlet />
+            </Suspense >
         </>
     )
 }
