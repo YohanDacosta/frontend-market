16cd5dd1853f37600b18b435b4f429f5f82266df
Yohan910 <yohan.dacosta910@gmail.com>
2024-08-27 15:11:56 +0200

    Updating Home Page

--- market/src/components/common/index.js
+++ market/src/components/common/index.js
@@ -1,7 +1,6 @@
-import Card from "../common/Card.jsx";
 import Loading from "../common/Loading.jsx";
 import CustomModal from "../common/CustomModal.jsx";
 import ButtonAction from "../common/ButtonAction.jsx";
 import CustomDropzone from "../common/CustomDropzone.jsx";
 
-export { Card, Loading, CustomModal, ButtonAction, CustomDropzone };
+export { Loading, CustomModal, ButtonAction, CustomDropzone };
